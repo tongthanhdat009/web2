bf15a561912230d39a404d36cb7334b25992ca82
tongthanhdat009 <84076965+tongthanhdat009@users.noreply.github.com>
2025-02-16 14:26:55 +0700

    test

---
+++ test/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; 2025 - Giao diện PHP cơ bản.</p>
+    </footer>
+</body>
+</html>
---
+++ test/form.php
@@ -0,0 +1,13 @@
+<div class="login-container">
+    <h2>Đăng Nhập</h2>
+    <form action="process.php" method="post">
+        <label for="username">Tên đăng nhập</label>
+        <input type="text" id="username" name="username" required>
+        
+        <label for="password">Mật khẩu</label>
+        <input type="password" id="password" name="password" required>
+        
+        <button type="submit">Đăng nhập</button>
+    </form>
+    <p>Chưa có tài khoản? <a href="#">Đăng ký</a></p>
+</div>
