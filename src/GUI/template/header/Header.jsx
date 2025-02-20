b1a48993327a835fb52063ba692d8a1da4747bd1
tongthanhdat009 <84076965+tongthanhdat009@users.noreply.github.com>
2025-02-20 15:40:37 +0700

    test

--- src/GUI/template/header/Header.jsx
+++ src/GUI/template/header/Header.jsx
@@ -1,38 +1,10 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import "./Header.css";
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">MyApp</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Trang chủ</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Giới thiệu</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Liên hệ</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <h1>Header</h1>
+    </div>
   );
 }
-
 export default Header;
