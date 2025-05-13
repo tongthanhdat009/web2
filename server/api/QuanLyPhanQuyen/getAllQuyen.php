8dc7ae2ad0604b28212800a583562dfac61e57d2
tongthanhdat009 <84076965+tongthanhdat009@users.noreply.github.com>
2025-05-13 22:32:09 +0700

    fix cors

--- server/api/QuanLyPhanQuyen/getAllQuyen.php
+++ server/api/QuanLyPhanQuyen/getAllQuyen.php
@@ -1,8 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Added OPTIONS for preflight requests
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true"); // Allow credentials
 
 include_once '../../config/Database.php';
 
